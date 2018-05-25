8f30eba2517bda1d698ac3bf684ae305b89fa615
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-05-25 11:32:49 -0400

    included hex .h file in test code

--- hex_to_uint_conversion.cpp
+++ hex_to_uint_conversion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "hexadecimal_colors_to_strings.h"
 
 int main() {
   std::cout << 0xFFFFFF << std::endl;
--- hexadecimal_colors_to_strings.h
+++ hexadecimal_colors_to_strings.h
@@ -1,7 +1,7 @@
-/* 
+/*
   Bunches of definitions for the WS2812 Breakout Board example code
-  
-*/ 
+
+*/
 
 // These are for the cascade function
 #define TOP_DOWN 0
@@ -11,7 +11,7 @@
   Standard HTML Color Codes sorted by Hex Value
     to see the colors in action, check out:
    http://www.w3schools.com/html/html_colorvalues.asp */
-   
+
 #define BLACK			0x000000
 #define NAVY			0x000080
 #define DARKBLUE		0x00008B
